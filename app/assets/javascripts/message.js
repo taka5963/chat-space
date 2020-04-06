626b0d4cbe6e2db9821550b4a2a141ac90f2f65e
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-04-06 15:47:04 +0900

    自動更新機能実装完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="chat-main__message-main__message-box">
+        `<div class="chat-main__message-main__message-box" data-message-id=${message.id}>
           <div class="chat-main__message-main__message-box__message-info">
             <div class="chat-main__message-main__message-box__message-info__message-name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="chat-main__message-main__message-box">
+        `<div class="chat-main__message-main__message-box" data-message-id=${message.id}>
           <div class="chat-main__message-main__message-box__message-info">
             <div class="chat-main__message-main__message-box__message-info__message-name">
               ${message.user_name}
@@ -39,6 +39,71 @@ $(function(){
       return html;
     };
   }
+  $(function() {
+    var reloadMessages = function() {
+      var last_message_id = $('.chat-main__message-main__message-box:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.chat-main__message-main').append(insertHTML);
+          $('.chat-main__message-main').animate({ scrollTop: $('.chat-main__message-main')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
+  });
+  //     var html =
+  //      `<div class="chat-main__message-main__message-box">
+  //         <div class="chat-main__message-main__message-box__message-info">
+  //           <div class="chat-main__message-main__message-box__message-info__message-name">
+  //             ${message.user_name}
+  //           </div>
+  //           <div class="chat-main__message-main__message-box__message-info__message-date">
+  //             ${message.created_at}
+  //           </div>
+  //         </div>
+  //         <div class="chat-main__message-main__message-box__message-detail">
+  //           <p class="chat-main__message-main__message-box__message-detail__content">
+  //             ${message.content}
+  //           </p>
+  //         </div>
+  //         <img src=${message.image} >
+  //       </div>`
+  //     return html;
+  //   } else {
+  //       var html =
+  //       `<div class="chat-main__message-main__message-box">
+  //           <div class="chat-main__message-main__message-box__message-info">
+  //             <div class="chat-main__message-main__message-box__message-info__message-name">
+  //               ${message.user_name}
+  //             </div>
+  //             <div class="chat-main__message-main__message-box__message-info__message-date">
+  //               ${message.created_at}
+  //             </div>
+  //           </div>
+  //           <div class="chat-main__message-main__message-box__message-detail">
+  //             <p class="chat-main__message-main__message-box__message-detail__content">
+  //               ${message.content}
+  //             </p>
+  //           </div>
+  //         </div>`
+  //       return html;
+  //   };
+  // }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -62,4 +127,5 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
 })
