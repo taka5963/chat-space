527d8303346ded330d041fee8fba14258751d196
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-04-06 16:03:21 +0900

    自動更新機能再提出

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -66,44 +66,6 @@ $(function(){
       setInterval(reloadMessages, 7000);
     }
   });
-  //     var html =
-  //      `<div class="chat-main__message-main__message-box">
-  //         <div class="chat-main__message-main__message-box__message-info">
-  //           <div class="chat-main__message-main__message-box__message-info__message-name">
-  //             ${message.user_name}
-  //           </div>
-  //           <div class="chat-main__message-main__message-box__message-info__message-date">
-  //             ${message.created_at}
-  //           </div>
-  //         </div>
-  //         <div class="chat-main__message-main__message-box__message-detail">
-  //           <p class="chat-main__message-main__message-box__message-detail__content">
-  //             ${message.content}
-  //           </p>
-  //         </div>
-  //         <img src=${message.image} >
-  //       </div>`
-  //     return html;
-  //   } else {
-  //       var html =
-  //       `<div class="chat-main__message-main__message-box">
-  //           <div class="chat-main__message-main__message-box__message-info">
-  //             <div class="chat-main__message-main__message-box__message-info__message-name">
-  //               ${message.user_name}
-  //             </div>
-  //             <div class="chat-main__message-main__message-box__message-info__message-date">
-  //               ${message.created_at}
-  //             </div>
-  //           </div>
-  //           <div class="chat-main__message-main__message-box__message-detail">
-  //             <p class="chat-main__message-main__message-box__message-detail__content">
-  //               ${message.content}
-  //             </p>
-  //           </div>
-  //         </div>`
-  //       return html;
-  //   };
-  // }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
